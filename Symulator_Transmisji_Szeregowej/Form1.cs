42ab2621b7c2736e01c8de29bba287bfd85feb5f
Weronika <weronika08wozniak@gmail.com>
2025-01-24 20:10:13 +0100

    dodano visualowe rzeczy

---
+++ Symulator_Transmisji_Szeregowej/Form1.cs
@@ -0,0 +1,10 @@
+namespace Symulator_Transmisji_Szeregowej
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
